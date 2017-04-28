aba09d3a52b252352baf11413f183ddbdd67b127
kamilsmuga <ksmuga@salesforce.com>
2017-04-28 17:02:08 +0100

    related links in refocus

--- refocus/load_subjects.sh
+++ refocus/load_subjects.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-YOUR_REFOCUS_URL=""
+YOUR_REFOCUS_URL="http://opensource-mon.herokuapp.com"
 
 jq -c '.projects[]' oscon_subjects.json | while read i; do
   curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d $i $YOUR_REFOCUS_URL/v1/subjects/OSCON2017/child
